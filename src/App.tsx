2540d46b3d006055b9355deba16b7c3dcc569d54
mbesirkesen <kesenmuhammedbesir@gmail.com>
2026-03-11 14:09:35 +0300

    feat: UI Kit sayfası ve /ui-kit rotası eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { ProjectDetail } from './pages/ProjectDetail'
+import { UIKit } from './pages/UIKit'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/proje/:id" element={<ProjectDetail />} />
+        <Route path="/ui-kit" element={<UIKit />} />
       </Routes>
     </BrowserRouter>
   )
